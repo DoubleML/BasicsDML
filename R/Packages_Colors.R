051ec95a31811e3b0456d1e53914bbf5ae055591
Philipp Bach <bach.philipp@outlook.com>
2022-06-01 12:19:35 +0200

    some styling

--- R/Packages_Colors.R
+++ R/Packages_Colors.R
@@ -1,18 +1,22 @@
-#Loading the required packages ####
-packages <- c("shiny","ggplot2", "EnvStats", "shinyWidgets","shinydashboard",
-              "latex2exp","dplyr", "plotly","MASS", "mvtnorm",
-              "data.table", "RColorBrewer")
-#installing new packages
+# Loading the required packages ####
+packages <- c(
+  "shiny", "ggplot2", "EnvStats", "shinyWidgets", "shinydashboard",
+  "latex2exp", "dplyr", "plotly", "MASS", "mvtnorm",
+  "data.table", "RColorBrewer"
+)
+# installing new packages
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-  install.packages(setdiff(packages, rownames(installed.packages())))  
+  install.packages(setdiff(packages, rownames(installed.packages())))
 }
-#load and attach packages
-if (length(setdiff(packages,names(sessionInfo()$otherPkgs))) > 0) {
-  lapply(setdiff(packages,names(sessionInfo()$otherPkgs)),
-         require, character.only = TRUE)
+# load and attach packages
+if (length(setdiff(packages, names(sessionInfo()$otherPkgs))) > 0) {
+  lapply(setdiff(packages, names(sessionInfo()$otherPkgs)),
+    require,
+    character.only = TRUE
+  )
 }
 
-#defining certain colors and methods ####
+# defining certain colors and methods ####
 dashboard_skin <- "black"
 loading_method <- "cube-grid"
 loading_color <- "#2980B9"
@@ -28,13 +32,15 @@ linecol4 <- "red"
 boxcol_1 <- "danger"
 boxcol_2 <- "primary"
 
-#logo ####
+# logo ####
 dbHeader <- dashboardHeader()
-dbHeader$children[[2]]$children <-  tags$a(href='https://docs.doubleml.org',
-                                           tags$img(src='DoubleML_Rhino_1000x1000.png',height='40',width='40'))
+dbHeader$children[[2]]$children <- tags$a(
+  href = "https://docs.doubleml.org",
+  tags$img(src = "DoubleML_Rhino_1000x1000.png", height = "40", width = "40")
+)
 
 # define the body ####
-body_col <- '/* logo */
+body_col <- "/* logo */
 .skin-blue .main-header .logo {
 background-color: #f4b943;
 }
@@ -70,20 +76,21 @@ background-color: #ff69b4;
 /* body */
 .content-wrapper, .right-side {
 background-color: #EAEDED;
-}'
+}"
 
 
 # Colors for plot
-col_palette = RColorBrewer::brewer.pal(11,"Spectral")
-colors = c("Non-orthogonal ML with sample splitting" = col_palette[1],
-           "Double ML (no sample splitting)" = col_palette[2],
-           "Non-orthogonal ML (no sample splitting)" = col_palette[3],
-           "DoubleML with underfitting learner and cross-fitting" = col_palette[4],
-           "Non-orthogonal ML with underfitting learner with sample splitting" = col_palette[5],
-           "Non-orthogonal ML with overfitting learner with sample splitting" = col_palette[6],
-           "Non-orthogonal ML with underfitting learner (no sample splitting)" = col_palette[9],
-           "DoubleML without cross-fitting" = col_palette[8],
-           "DoubleML with cross-fitting" = col_palette[9],
-           "Double ML with overfitting learner (no sample splitting)" = col_palette[10],
-           "Double ML with underfitting learner (no sample splitting)" = col_palette[11]
+col_palette <- RColorBrewer::brewer.pal(11, "Spectral")
+colors <- c(
+  "Non-orthogonal ML with sample splitting" = col_palette[1],
+  "Double ML (no sample splitting)" = col_palette[2],
+  "Non-orthogonal ML (no sample splitting)" = col_palette[3],
+  "DoubleML with underfitting learner and cross-fitting" = col_palette[4],
+  "Non-orthogonal ML with underfitting learner with sample splitting" = col_palette[5],
+  "Non-orthogonal ML with overfitting learner with sample splitting" = col_palette[6],
+  "Non-orthogonal ML with underfitting learner (no sample splitting)" = col_palette[9],
+  "DoubleML without cross-fitting" = col_palette[8],
+  "DoubleML with cross-fitting" = col_palette[9],
+  "Double ML with overfitting learner (no sample splitting)" = col_palette[10],
+  "Double ML with underfitting learner (no sample splitting)" = col_palette[11]
 )
--- server.R
+++ server.R
@@ -1,92 +1,111 @@
-#Shiny App for visualization 
+# Shiny App for visualization
 
-#Loading the required colors and packages from the R subdirectory ####
+# Loading the required colors and packages from the R subdirectory ####
 
-# Estimation results saved in the next 
+# Estimation results saved in the next
 source("R/basics_estimation.R")
 
 function(input, output) {
-  
   output$dens_ingrds <- renderPlot({
     if (input$orth) {
       if (input$samplesplit) {
         if (input$hq == "Tuned") {
-          est_selection = c("theta_dml")
+          est_selection <- c("theta_dml")
         } else if (input$hq == "Underfitting") {
-          est_selection = c("theta_dml_underfit")
+          est_selection <- c("theta_dml_underfit")
         } else if (input$hq == "Overfitting") {
-          est_selection = c("theta_dml_overfit")
+          est_selection <- c("theta_dml_overfit")
         }
       } else {
         if (input$hq == "Tuned") {
-          est_selection = c("theta_orth_nosplit")
+          est_selection <- c("theta_orth_nosplit")
         } else if (input$hq == "Underfitting") {
-          est_selection = c("theta_dml_nosplit_underfit")
+          est_selection <- c("theta_dml_nosplit_underfit")
         } else if (input$hq == "Overfitting") {
-          est_selection = c("theta_dml_nosplit_overfit")
+          est_selection <- c("theta_dml_nosplit_overfit")
         }
       }
     } else {
       if (input$samplesplit) {
         if (input$hq == "Tuned") {
-          est_selection = c("theta_nonorth")
+          est_selection <- c("theta_nonorth")
         } else if (input$hq == "Underfitting") {
-          est_selection = c("theta_nonorth_underfit")
+          est_selection <- c("theta_nonorth_underfit")
         } else if (input$hq == "Overfitting") {
-          est_selection = c("theta_nonorth_overfit")
+          est_selection <- c("theta_nonorth_overfit")
         }
       } else {
         if (input$hq == "Tuned") {
-          est_selection = c("theta_nonorth_nosplit")
+          est_selection <- c("theta_nonorth_nosplit")
         } else if (input$hq == "Underfitting") {
-          est_selection = c("theta_nonorth_nosplit_underfit")
+          est_selection <- c("theta_nonorth_nosplit_underfit")
         } else if (input$hq == "Overfitting") {
-          est_selection = c("theta_nonorth_nosplit_overfit")
+          est_selection <- c("theta_nonorth_nosplit_overfit")
         }
       }
     }
-    
-    data = estimates_scaled[estimation == est_selection, ]
-    
-    g_dens = ggplot(data) +
-      geom_histogram(aes(y=..density.., x=coef, fill = label,
-                         col = label),
-                     bins = 30, alpha = 0.6) +
+
+    data <- estimates_scaled[estimation == est_selection, ]
+
+    g_dens <- ggplot(data) +
+      geom_histogram(aes(
+        y = ..density.., x = coef, fill = label,
+        col = label
+      ),
+      bins = 30, alpha = 0.6
+      ) +
       geom_vline(aes(xintercept = 0), col = "black") +
       suppressWarnings(geom_function(fun = dnorm, colour = "darkgrey")) +
-      scale_color_manual(name = '',
-                         values =  colors) +
-      scale_fill_manual(name = '',
-                        values = colors) +
-      xlim(c(-6.0, 6.0)) + xlab("") + ylab("") + theme_minimal() + 
-      theme(legend.position="none")
-    
+      scale_color_manual(
+        name = "",
+        values = colors
+      ) +
+      scale_fill_manual(
+        name = "",
+        values = colors
+      ) +
+      xlim(c(-6.0, 6.0)) +
+      xlab("") +
+      ylab("") +
+      theme_minimal() +
+      theme(legend.position = "none")
+
     g_dens
   })
-  
+
   output$dens_cf <- renderPlot({
     if (!input$crossfit) {
-      est_selection = c("theta_dml_nocf")
+      est_selection <- c("theta_dml_nocf")
     } else {
-      est_selection = c("theta_dml")
+      est_selection <- c("theta_dml")
     }
-    
-    data = estimates_unscaled[estimation == est_selection, ]
-    
-    g_dens_unscaled = ggplot(data) +
-      geom_histogram(aes(y=..density.., x=coef, fill = label,
-                         col = label),
-                     bins = 30, alpha = 0.9) +
+
+    data <- estimates_unscaled[estimation == est_selection, ]
+
+    g_dens_unscaled <- ggplot(data) +
+      geom_histogram(aes(
+        y = ..density.., x = coef, fill = label,
+        col = label
+      ),
+      bins = 30, alpha = 0.9
+      ) +
       geom_vline(aes(xintercept = 0), col = "black") +
       # suppressWarnings(geom_function(fun = dnorm, colour = "darkgrey")) +
-      scale_color_manual(name = '',
-                         values =  colors) +
-      scale_fill_manual(name = '',
-                        values = colors) +
-      xlim(c(-0.25, 0.25)) + ylim(c(0,9)) + xlab("") + ylab("") + theme_minimal() +
-      theme(legend.position="none")
-    
+      scale_color_manual(
+        name = "",
+        values = colors
+      ) +
+      scale_fill_manual(
+        name = "",
+        values = colors
+      ) +
+      xlim(c(-0.25, 0.25)) +
+      ylim(c(0, 9)) +
+      xlab("") +
+      ylab("") +
+      theme_minimal() +
+      theme(legend.position = "none")
+
     g_dens_unscaled
-    
   })
 }
